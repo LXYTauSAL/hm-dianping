3c8f46c93acb0c54a4e0e47a3d6fd2c37ec18080
lxy <1223240287@qq.com>
2022-12-01 22:00:26 +0800

    hot-fix commit2

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -27,6 +27,7 @@ class HmDianPingApplicationTests {
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
+        System.out.println("hot-fix test");
         CountDownLatch latch = new CountDownLatch(300);
 
         Runnable task = () ->{
